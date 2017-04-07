b0823ee1506f9b20daefffcd4e0d4e1d40b61290
Ömer Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2017-04-07 19:11:48 +0300

    Login feature added.

---
+++ AspNetMvcClaimsSample.WebApp/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMvcClaimsSample.WebApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
